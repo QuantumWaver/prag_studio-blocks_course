c956463eda5efc85cd8f4df8c46d7ecda8e56093
QuantumWaver <sholl76@gmail.com>
2017-09-09 16:16:27 -0400

    Add chapter 2 exercises

--- block_basics.rb
+++ block_basics.rb
@@ -1,1 +1,12 @@
 puts "Let's start mastering Ruby blocks!"
+
+puts "\ntimes"
+3.times { |i| puts "Rush #{i}" }
+
+puts "\nupto:"
+1.upto(3) { |i| puts "Rush #{i}" }
+
+var = 2112
+puts "\nstep"
+1.step(9, 2) { |i| puts "Rush #{i} #{var}" }
+
---
+++ flyer.rb
@@ -0,0 +1,20 @@
+class Flyer
+  attr_reader :name, :email, :miles_flown
+
+  def initialize(name, email, miles_flown)
+    @name = name
+    @email = email
+    @miles_flown = miles_flown
+  end
+
+  def to_s
+    "#{name} (#{email}): #{miles_flown}"
+  end
+end
+
+flyers = []
+1.upto(5) do |i|
+  flyers << Flyer.new("Flyer #{i}", "Flyer#{i}@email.com", i**6)
+end
+
+puts flyers
